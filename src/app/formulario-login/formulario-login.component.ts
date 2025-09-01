2fd722630977dd2b2e4ec783138f29a02c5ebdbb
EnricoMustafa <enricobrasil123@gmail.com>
2025-09-01 15:06:17 -0300

    fit novas funcionalidades

--- src/app/formulario-login/formulario-login.component.ts
+++ src/app/formulario-login/formulario-login.component.ts
@@ -1,41 +1,51 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Usuario } from '../cadastrar-login/usuario.model';
 import { Logar } from '../services/logar.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-formulario-login',
-  imports: [
-    RouterLink,
-    FormsModule,
-  ],
+  imports: [RouterLink, FormsModule],
   templateUrl: './formulario-login.component.html',
-  styleUrl: './formulario-login.component.css'
+  styleUrl: './formulario-login.component.css',
 })
 export class FormularioLoginComponent {
-  constructor(
-    private logar: Logar,
-    private router: Router
-  ){}
+  constructor(private logar: Logar, private router: Router) {}
 
-  email:string = '';
-  senha:string = '';
-
+  email: string = '';
+  senha: string = '';
   mensagemErro: string = '';
-  
-    mensagemSucesso(){
-      if(this.logar.validarLogin(this.email,this.senha) === true){
-        console.log("Validação deu certo");
-        this.router.navigate(['/lista'])        
-      }else{
-        console.log("Email ou senha incorreto");
-        this.mensagemErro = 'Usuario ou senha incorreta!'
-        setTimeout(() => {
-          this.mensagemErro = ''
-        }, 1000)
-      }
+
+  mensagemSucesso() {
+    if (this.logar.validarLogin(this.email, this.senha) === true) {
+      console.log('Validação deu certo');
+      alert("Seja bem vindo!");
+      this.router.navigate(['/lista']);
+
+    } else if (this.mensagemErro === '') {
+      this.mensagemErro = 'Preencha os campos vazios, usuario e senha';
+      setTimeout(() => {
+        this.mensagemErro = '';
+      }, 5000);
+    } else {
+      console.log('Email ou senha incorreto');
+      this.mensagemErro = 'Usuario ou senha incorreta!';
+      setTimeout(() => {
+        this.mensagemErro = '';
+      }, 5000);
+    }
   }
+
+  // ****** A FAZER ******
+  // pegarNome(){
+  //   const data = localStorage.getItem('usuariosss');
+
+  //   if(data){
+  //     const usuario = JSON.parse(data);
+  //     this.nome = usuario.nome
+  //     console.log("Seja bem vindo! " +this.nome);
+  //   }
+  // }
 }
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Usuario } from '../cadastrar-login/usuario.model';
+import { CadastroService } from '../services/cadastro.service';
 
 @Component({
   selector: 'app-lista',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
-
+  constructor(private user: CadastroService){}
+  usuario = new Usuario();
 }
+
